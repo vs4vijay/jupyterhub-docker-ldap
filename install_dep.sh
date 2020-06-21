c0ed2c5d7d601a31ba7bfcfc8a5dc2b030018a48
vs4vijay <vs4vijay@gmail.com>
2020-06-21 17:01:47 +0530

    Fix dockerfile

--- install_dep.sh
+++ install_dep.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 # Installing Dependencies to Jupyter
+
+pip --version
+
+pip3 --version
+
+pip3 install dockerspawner ldapauthenticator
+
 python3 -m pip install --no-cache --user dockerspawner ldapauthenticator
